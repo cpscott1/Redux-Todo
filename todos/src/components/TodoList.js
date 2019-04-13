7948a9853e73c1b106ec8bf9ae4328f5e8adf172
cpscott1 <thacam18@gmail.com>
2019-04-13 12:56:44 -0500

    add reducer

---
+++ todos/src/components/TodoList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addNewTodo, toggleTodo } from '../actions';
+
+class TodoList extends Component {
+  state = {
+    newTodo: ''
+  }
+}
+
+changeHandler = event => {
+  this.setState({
+    newTodo: event.target.value
+  })
+}
+
+addTodo = event => {
+  event.preventDefault();
+  this.props.addNewTodo(this.state.newTodo)
+};
+
+toggleTodo = (event, index) => {
+  event.preventDefault();
+  this.props.toggleTodo(index);
+}
+
+render() {
+  return (
+    <>
+      <div className='todo-list'>
+        {this.props.todos.map((todo, index) => (
+          <h4 onClick={event => this.toggleTodo(event, index)} key={index}>
+          
+          </h4>
+        )}
+      </div>
+    </>
+  )
+}
+
+const mapStateToProps = state => ({
+  todos: state.todosReducer.todos
+})
+
+export default connect(
+  mapStateToProps,
+  { addNewTodo, toggleTodo }
+)(TodoList);
--- todos/src/reducers/index.js
+++ todos/src/reducers/index.js
@@ -1,5 +1,29 @@
-
+import { ADD_TODO, TOGGLE_TODO } from '../actions'
 
 const initialState = {
   todos: []
+}
+
+export const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      const newTodo = {
+        value: action.payload,
+        completed: false
+      };
+      return {
+        ...state,
+        todos: [...state.todos, newTodo]
+      }
+      case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo, index) =>
+          action.payload === index ? {...todos, completed: !todo.completed}
+          : todo
+      )
+      }
+      default:
+      return state;
+  }
 }
